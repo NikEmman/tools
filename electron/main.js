14a922cdbf74a9e62f9f073da083244e316bbb8f
NikEmman <121699424+NikEmman@users.noreply.github.com>
2026-05-02 14:59:03 +0300

    Add electron wrapper

---
+++ electron/main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow, protocol, net } = require("electron");
+const path = require("path");
+const url = require("url");
+const fs = require("fs");
+
+protocol.registerSchemesAsPrivileged([
+  {
+    scheme: "app",
+    privileges: {
+      secure: true,
+      standard: true,
+      supportFetchAPI: true,
+    },
+  },
+]);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadURL("app://./index.html");
+}
+
+app.whenReady().then(() => {
+  protocol.handle("app", (request) => {
+    let filePath = request.url.slice("app://./".length).split("?")[0];
+    let fullPath = path.join(__dirname, "..", filePath);
+
+    if (!fs.existsSync(fullPath) || fs.statSync(fullPath).isDirectory()) {
+      fullPath = path.join(fullPath, "index.html");
+    }
+
+    return net.fetch(url.pathToFileURL(fullPath).toString());
+  });
+
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
